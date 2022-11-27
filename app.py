cf1bca2871a6316ad43d1bf7e5bb29d31ebdc2b6
jinhwan7 <rkflql851@gmail.com>
2022-11-27 15:01:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, make_response
 from pymongo import MongoClient  # 패키지 임포트
 
 # client = MongoClient("mongodb+srv://test:sparta@cluster0.jmcjmfs.mongodb.net/?retryWrites=true&w=majority")
@@ -13,7 +13,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # return render_template('main.html')
     return render_template('index.html')
 
 #로그인
@@ -22,11 +21,27 @@ def login():
     return render_template('login.html')
 @app.route('/login', methods=['POST'])
 def login_post():
-    name_receive = request.form['name_give']
-    return jsonify({'msg':'post연결완료'})
-@app.route('/login', methods=['GET'])
-def login_get():
-    return jsonify({'msg':'get연결완료'})
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    users_list = list(db.users.find({},{'_id':False,'name':False}))
+    for id_pw in users_list:
+        if id_receive == id_pw['id'] and pw_receive == id_pw['pw']:
+            user_id = request.form["id_give"]
+            res = make_response(jsonify({'msg':"로그인성공"}))
+            res.set_cookie("user_id", user_id)
+            return res
+    return jsonify({'msg':'아이디나 비밀번호가틀립니다'})
+
+@app.route('/getcookie')
+def getcookie():
+    user_id = request.cookies.get("user_id")
+    return user_id
+
+@app.route('/delcookie',methods=['GET'])
+def delCookie():
+    res = make_response(jsonify({'msg':"로그아웃성공"}))
+    res.delete_cookie('user_id')
+    return res
 
 #회원가입
 @app.route('/signup')
@@ -44,11 +59,8 @@ def signup_post():
     }
     db.users.insert_one(doc)
     return jsonify({'msg':'회원가입완료'})
-@app.route('/signup', methods=['GET'])
-def signup_get():
-    return jsonify({'msg':'get연결완료'})
-@app.route('/idcheck', methods=['POST'])
-def id_check_post():
+@app.route('/idcheck', methods=['GET'])
+def id_check_get():
     id_list = list(db.users.find({},{'_id':False,'pw':False,'name':False}))
     return jsonify({'id_list':id_list})
 
@@ -125,4 +137,4 @@ def do_ask():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
