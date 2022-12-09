0ada8b50bf4f5a566128dd5cce62dc1215f2246f
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2022-12-09 13:44:01 +0900

    프로젝트 기본 셋팅
    몽고디비, index.html 등

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+
+app = Flask(__name__)
+
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://test:sparta@cluster0.vnmg85w.mongodb.net/?retryWrites=true&w=majority")
+db = client.dbsparta
+
+
+
+
+
+@app.route('/')
+def home():
+
+    doc = {'name': 'bobby', 'age': 21}
+    db.test.insert_one(doc)
+
+    return 'index.html'
+
+
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
