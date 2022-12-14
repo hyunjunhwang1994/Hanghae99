cce05508a035ced181e93a4680bb12a0be03e2a0
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2022-12-14 17:23:55 +0900

    prac03_3 생성

---
+++ Prac03_2.java
@@ -0,0 +1,61 @@
+public class Prac03_2 {
+
+
+    public static void main(String[] args) {
+
+//        int x = -100;
+//
+//        // +부호의 경우 현재 값 유지
+//        // -부호의 경우 음수는 양수로, 양수는 음수로 연산됨.
+//        int result1 = +x;
+//        int result2 = -x;
+//        // 부호연산의 결과는 int 타입
+//        System.out.println(result1);
+//        System.out.println(result2);
+//
+//
+//        byte b = 100;
+//        //byte result3 = -b; //부호 연산시 Int값으로 반환되므로 컴파일 에러
+//        int result3 = -b;
+//        System.out.println(result3);
+
+
+//        int x = 1;
+//        int y = 1;
+//
+//        int result1 = ++x + 10;
+//        int result2 = y++ + 10;
+//        System.out.println(result1);
+//        System.out.println(result2);
+
+//        boolean play = true;
+//        System.out.println(play);
+//
+//        play = !play;
+//        System.out.println(play);
+//
+//        play = !play;
+//        System.out.println(play);
+
+//        String str1 = "JDK" + 6.0;
+//        String str2 = str1 + " 특징";
+//        System.out.println(str2);
+//
+//        String str3 = "JDK" + 3 + 3.0;
+//        String str4 = 3 + 3.0 + "JDK";
+//        System.out.println(str3);
+//        System.out.println(str4);
+
+//        String strVar1 = "현준";
+//        String strVar2 = "현준";
+//
+//        System.out.println(strVar1.equals(strVar2));
+//        System.out.println(!strVar1.equals(strVar2));
+//
+//        // 십의자리수 이하 버리기
+//        int value = 356;
+//        System.out.println(value / 100 * 100);
+
+    }
+
+}
---
+++ Prac03_3.java
@@ -0,0 +1,6 @@
+public class Prac03_3 {
+
+    public static void main(String[] args) {
+
+    }
+}
