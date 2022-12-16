b40313d8c38b6a65bdfa049dd1fca934ac40bb1c
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2022-12-16 21:04:26 +0900

    q16 clear

---
+++ q16.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+
+public class q16 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    public static int[] solution(int[] numbers){
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        int len = numbers.length;
+        int temp;
+
+        for(int i=0; i<len-1; i++){
+
+            for(int j=i+1; j<len; j++){
+                temp = numbers[i] + numbers[j];
+                if (arr.indexOf(temp) < 0) {
+                    arr.add(temp);
+                }
+            }
+
+        }
+
+        int answer[] = new int[arr.size()];
+        for(int i=0;i<answer.length; i++)
+            answer[i] = arr.get(i);
+        Arrays.sort(answer);
+        return answer;
+
+    }
+}
