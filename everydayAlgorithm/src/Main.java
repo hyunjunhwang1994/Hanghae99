29939454f7cd789007e413663e25b9d717a29889
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2022-12-23 21:13:34 +0900

    Fibonacci number Clear

--- everydayAlgorithm/src/Main.java
+++ everydayAlgorithm/src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    // 12월 23일
+    //https://school.programmers.co.kr/learn/courses/30/lessons/12945
+    // 피보나치 수 n % 1234567
+
+
+    public int solution(int n){
+        int answer = 0;
+
+        int n0 = 1;
+        int n1 = 1;
+
+
+
+        for(int i = 2; i<n; i++){
+
+            answer = (n0 + n1) % 1234567;
+            n0 = n1;
+            n1 = answer;
+
+        }
+
+
+        return answer;
     }
+
+
+    public static void main(String[] args) {
+
+        Main test = new Main();
+
+
+        System.out.println(test.solution(100000));
+
+
+    }
+
+
+
 }
