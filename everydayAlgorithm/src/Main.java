f1eb399fde34f21ac38c45179fef08a159f2e608
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2022-12-23 21:53:04 +0900

    test

--- everydayAlgorithm/src/Main.java
+++ everydayAlgorithm/src/Main.java
@@ -11,8 +11,6 @@ public class Main {
         int n0 = 1;
         int n1 = 1;
 
-
-
         for(int i = 2; i<n; i++){
 
             answer = (n0 + n1) % 1234567;
@@ -29,10 +27,7 @@ public class Main {
     public static void main(String[] args) {
 
         Main test = new Main();
-
-
         System.out.println(test.solution(100000));
-
 
     }
 
