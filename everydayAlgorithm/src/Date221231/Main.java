3b2a0b5e7dd654454656d17ae8b5dac29b804f65
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2022-12-31 12:38:36 +0900

    matrix multiplication clear

---
+++ everydayAlgorithm/src/Date221231/Main.java
@@ -0,0 +1,88 @@
+package Date221231;
+
+
+// 2022 12월 31일
+// https://school.programmers.co.kr/learn/courses/30/lessons/12949
+// 행렬의 곱셈.
+
+
+public class Main {
+
+
+
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+
+
+
+        int arr1row = arr1.length;
+        int arr2col = arr2[0].length;
+        int sum = 0;
+
+        int[][] answer = new int[arr1row][arr2col];
+
+
+        for (int i = 0; i<arr1row; i++){
+
+            for (int j = 0; j < arr2col; j++) {
+
+                // [0][0] * [0][0]
+                // [0][1] * [1][0]
+                // [0][2] * [2][0]
+
+                // [0][0] * [0][1]
+                // [0][1] * [1][1]
+                // [0][2] * [2][1]
+
+                // [0][0] * [0][2]
+                // [0][1] * [1][2]
+                // [0][2] * [2][2]
+
+                // [1][0] * [0][0]
+                // [1][1] * [1][0]
+                // [1][2] * [2][0]
+
+                // [1][0] * [0][1]
+                // [1][1] * [1][1]
+                // [1][2] * [2][1]
+
+                // [1][0] * [0][2]
+                // [1][1] * [1][2]
+                // [1][2] * [2][2]
+
+
+                System.out.println(i + "row, " +  j + "column");
+                for (int k = 0; k < arr2.length; k++) {
+
+
+                     sum += arr1[i][k] * arr2[k][j];
+
+
+                }
+                answer[i][j] = sum;
+                sum = 0;
+
+            }
+
+
+        }
+
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] arr2 = {{3, 3}, {3, 3}};
+
+
+        Main main = new Main();
+
+        System.out.println(main.solution(arr1, arr2));
+
+
+
+
+    }
+}
